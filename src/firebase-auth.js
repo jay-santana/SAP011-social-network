3dc7419f9345468dd7d56c429cd5a6eb7dd6f680
jay santana <jayannyrayara@gmail.com>
2023-09-13 09:39:42 -0300

    Autenticação login funcionando

--- src/firebase-auth.js
+++ src/firebase-auth.js
@@ -5,31 +5,32 @@ import { auth } from './firebase-conf.js';
 
 //Criação de novos usuários
 export function createUser(user, email, password) {
-createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
 
-  // updateProfile(user, { displayName });
-  // Signed in 
-    const user = userCredential.user;
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    
-  });
+      // updateProfile(user, { displayName });
+      // Signed in 
+      const user = userCredential.user;
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+
+    });
 }
 
 //Login de usuários existentes
 export function signIn(email, password) {
   signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
-  // Signed in 
+      // Signed in 
       const user = userCredential.user;
     })
     .catch((error) => {
       const errorCode = error.code;
       const errorMessage = error.message;
     });
+  return signInWithEmailAndPassword(auth, email, password);
 }
 
 //Para desconectar um usuário
